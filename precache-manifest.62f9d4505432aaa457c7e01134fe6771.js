8452f7046b9a6617c29f5ba28102511855ae45c4
Grigori Kartashyan <gkartashyan@vmware.com>
2020-01-10 17:35:05 +0400

    react app test

---
+++ precache-manifest.62f9d4505432aaa457c7e01134fe6771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9763f2d6e28c78ac0be6b32b0988f4bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ac8d654c7e2a45d6f1c",
+    "url": "/static/css/main.7a7b2ebb.chunk.css"
+  },
+  {
+    "revision": "83df6ec9799609328002",
+    "url": "/static/js/2.4fd76ae1.chunk.js"
+  },
+  {
+    "revision": "4ac8d654c7e2a45d6f1c",
+    "url": "/static/js/main.5c31e35b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
